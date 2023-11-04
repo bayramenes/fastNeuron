68f8b7b6582923388a35862f29925d548cb1d71c
bayramenes <bayram.enes1928@gmail.com>
2023-11-04 17:32:21 +0300

    added some pretty printing for better output

--- loss_funcs.py
+++ loss_funcs.py
@@ -5,9 +5,12 @@ import numpy as np
 
 class BinaryCrossEntropy:
 
+    def __repr__(self) -> str:
+        return "Binary Cross Entropy"
+
     @staticmethod 
     def calculate(output:np.ndarray,labels:np.ndarray) -> np.float64:
-        return -1 / output.shape[0] * np.sum(labels * np.log(output) + (1 - labels) * np.log(1 - output))
+        return -1 / output.shape[0] * np.sum(labels * np.log(output + 1e-15) + (1 - labels) * np.log(1 - output + 1e-15))
     
     
     @staticmethod
@@ -17,6 +20,8 @@ class BinaryCrossEntropy:
 
 
 class CategoricalCrossEntropy:
+    def __repr__(self) -> str:
+        return "Categorical Cross Entropy"
     @staticmethod 
     def calculate(output:np.ndarray,labels:np.ndarray) -> np.float64:
         pass
@@ -28,6 +33,8 @@ class CategoricalCrossEntropy:
 
 
 class MSE:
+    def __repr__(self) -> str:
+        return "Mean Squared Error"
     @staticmethod 
     def calculate(output:np.ndarray,labels:np.ndarray) -> np.float64:
         return (1/2) * np.mean(np.square(output - labels))
