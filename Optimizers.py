670ab3f60a2890ee873999b61aabc4cf3e48833e
bayramenes <bayram.enes1928@gmail.com>
2023-11-04 17:34:21 +0300

    changed optimizers from static methods in a class to seperate classes and added docstring and printing for better output

--- Optimizers.py
+++ Optimizers.py
@@ -1,21 +1,94 @@
 # this is a file that contains a class that has different types of optimizers 
 
-from layer import layer
 
 import numpy as np
-class Optimizers:
+class MiniBatchGradientDescent:
+    pass
+
+class StochasticGradientDescent:
+    """
+    implemenation of the stochastic gradient descent algorithm
+    :param model: neural network object
+    :param loss: loss function object
+    :param X: training examples
+    :param Y: training labels
+    :param learning_rate: learning rate
+    :param epochs: number of epochs
+    :return: the model and the costs of the model
+    """
+    def __call__(
+                self,
+                model:object, 
+                loss:object,
+                X:np.ndarray,
+                Y:np.ndarray,
+                learning_rate:float,
+                epochs:int
+            ) -> tuple[object:list[np.float64]]:
+        
+        costs=[]
+
+        # do as many epochs as needed
+        for epoch in range(epochs):
+            # go over all of the training examples in each epoch
+            for i in range(X.shape[0]):
+                model.outputs = []
+                output = model.forward(X[i].reshape(1,X.shape[1]))
+                
+                initial_derivatives = loss.initial_derivative(output,Y[i].reshape(1,1))
+
+                for index in range(len(model.layers) - 1 , -1 ,-1):
+                    model.layers[index].weights = model.layers[index].weights - learning_rate *  np.matmul(model.outputs[index].T,initial_derivatives)
+                    model.layers[index].biases = model.layers[index].biases - learning_rate * np.sum(initial_derivatives , axis= 0)
+                    initial_derivatives = np.matmul(initial_derivatives,model.layers[index].weights.T)
+                    positive_values_derivative = (model.outputs[index] > 0).astype(int)
+                    if model.layers[index].activation == "sigmoid": initial_derivatives *= model.outputs[index] * (1 - model.outputs[index])
+                    elif model.layers[index].activation == "relu": initial_derivatives *= positive_values_derivative
+                    elif model.layers[index].activation == "leaky-relu": initial_derivatives *= np.where(positive_values_derivative == 0,0.1,positive_values_derivative)
+                    elif model.layers[index].activation == "tanh": initial_derivatives *= 1 - (model.outputs[index] ** 2)
+
+            
+            if epoch % (epochs // 100) == 0:
+                cost = loss.calculate(model.forward(X),Y)
+                costs.append(cost)
+            if epoch % (epochs // 10) == 0:
+                accuracy = ( (X.shape[0] - np.sum((model.predict(X) != Y).astype(int))) / X.shape[0] ) * 100
+                print(f"{epoch} cost : {cost} accuracy : {round(accuracy,ndigits=2)}%")
+
+
+
+        cost = loss.calculate(model.forward(X),Y)
+        accuracy = ( (X.shape[0] - np.sum((model.predict(X) != Y).astype(int))) / X.shape[0] ) * 100
+        print(f" cost : {cost} accuracy : {round(accuracy,ndigits=2)}%")
+
+        return model,costs
     
-    @staticmethod
-    def BatchGradientDescent(
-            model:object, # neural network object
-            loss:object, #loss function object 
-            X:np.ndarray,
-            Y:np.ndarray,
-            learning_rate:float,
-            epochs:int
-        ) -> tuple[object,list[np.float64]]:
+    def __repr__(self) -> str:
+        return "Stochastic Gradient Descent"    
 
 
+class BatchGradientDescent:
+    """
+    implementation of the batch gradient descent algorithm
+    :param model: neural network object
+    :param loss: loss function object
+    :param X: training examples
+    :param Y: training labels
+    :param learning_rate: learning rate
+    :param epochs: number of epochs
+    :return: the model and the costs of the model
+    """
+    def __call__(
+                    self, 
+                    model:object, # neural network object
+                    loss:object, #loss function object 
+                    X:np.ndarray,
+                    Y:np.ndarray,
+                    learning_rate:float,
+                    epochs:int
+
+                 ) -> tuple[object,list[np.float64]]:
+        
         # NOTE: this algorithm can be implemented using recursion or using iterations
         #  i chose to use iterations for performance purposes...
 
@@ -63,77 +136,7 @@ class Optimizers:
         print(f" cost : {cost} accuracy : {round(accuracy,ndigits=2)}%")
 
         return model,COSTS
-                
-                
+    
 
-
-
-
-    def MiniBatchGradientDescent(X:np.ndarray,Y:np.ndarray,learning_rate:float,epochs:int,batches:int):
-        pass
-
-    def StochasticGradientDescent(
-                model:object, 
-                loss:object,
-                X:np.ndarray,
-                Y:np.ndarray,
-                learning_rate:float,
-                epochs:int
-            ) -> tuple[object:list[np.float64]]:
-        
-        COSTS=[]
-
-        # do as many epochs as needed
-        for epoch in range(epochs):
-            # go over all of the training examples in each epoch
-            for i in range(X.shape[0]):
-                model.outputs = []
-                output = model.forward(X[i].reshape(1,X.shape[1]))
-                # print(f'output: {output}')
-                # print(f"output.shape: {output.shape}")
-                # print(f"Y[i]: {Y[i]}")
-                # print(f"Y[i].shape: {Y[i].shape}")
-
-                initial_derivatives = loss.initial_derivative(output,Y[i].reshape(1,1))
-
-                # print(f"initial_derivatives: {initial_derivatives}")
-                # print(f"initial_derivatives.shape: {initial_derivatives.shape}")
-                # update the layers from last to first
-                for index in range(len(model.layers) - 1 , -1 ,-1):
-                    # print(f"model.outputs[index] : {model.outputs[index]}")
-                    # print(f"model.outputs[index].shape : {model.outputs[index].shape}")
-                    model.layers[index].weights = model.layers[index].weights - learning_rate *  np.matmul(model.outputs[index].T,initial_derivatives)
-                    model.layers[index].biases = model.layers[index].biases - learning_rate * np.sum(initial_derivatives , axis= 0)
-                    initial_derivatives = np.matmul(initial_derivatives,model.layers[index].weights.T)
-                    positive_values_derivative = (model.outputs[index] > 0).astype(int)
-                    if model.layers[index].activation == "sigmoid": initial_derivatives *= model.outputs[index] * (1 - model.outputs[index])
-                    elif model.layers[index].activation == "relu": initial_derivatives *= positive_values_derivative
-                    elif model.layers[index].activation == "leaky-relu": initial_derivatives *= np.where(positive_values_derivative == 0,0.1,positive_values_derivative)
-                    elif model.layers[index].activation == "tanh": initial_derivatives *= 1 - (model.outputs[index] ** 2)
-
-                # model.summary()
-                # break
-            
-            if epoch % (epochs // 100) == 0:
-                cost = loss.calculate(output,Y)
-                COSTS.append(cost)
-            if epoch % (epochs // 10) == 0:
-                accuracy = ((X.shape[0] - np.sum(((output >= 0.5).astype(int) != Y).astype(int))) / X.shape[0] ) * 100
-                print(f"{epoch} cost : {cost} accuracy : {round(accuracy,ndigits=2)}%")
-
-            # break
-
-
-        cost = loss.calculate(output,Y)
-        accuracy = ((X.shape[0] - np.sum(((output >= 0.5).astype(int) != Y).astype(int))) / X.shape[0] ) * 100
-        print(f" cost : {cost} accuracy : {round(accuracy,ndigits=2)}%")
-
-        return model,COSTS
-
-
-                
-
-
-
-
-
+    def __repr__(self) -> str:
+        return "Batch Gradient Descent"               
