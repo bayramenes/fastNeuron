5861e696f00b8dd04d43fc5fa89b5ea4a492afba
bayramenes <bayram.enes1928@gmail.com>
2023-11-04 17:33:21 +0300

    removed some unncessary code and added some docstring to the methods

--- network.py
+++ network.py
@@ -1,7 +1,7 @@
 # this is a class that initiates a neural network and gives high level control over
 # it
 from loss_funcs import *
-from Optimizers import Optimizers
+from Optimizers import *
 from layer import layer
 import numpy as np
 
@@ -14,7 +14,7 @@ class neural_network:
         self.outputs = []
 
         # default optimizer and loss functions
-        self.optimizer = Optimizers.BatchGradientDescent
+        self.optimizer = BatchGradientDescent()
         self.cost = MSE()
 
     # accept a list of layers and set as the layers of the neural network
@@ -25,11 +25,9 @@ class neural_network:
 
     def forward(self, inputs: np.ndarray):
         # iterate through the layers
-
-        for layer in self.layers:
+        for layer_ in self.layers:
             self.outputs.append(inputs)
-            inputs = layer.forward(inputs)
-
+            inputs = layer_.forward(inputs)
         # we have to add the outputs of the last layer manually
         self.outputs.append(inputs)
         return inputs
@@ -37,43 +35,8 @@ class neural_network:
     def get_weights(self):
         return [layer_.weights for layer_ in self.layers]
 
-    # def backward(self, initial_derivatives: np.ndarray):
-    #     # for now i will assume it is only one unit so i will apply binary cross-entropy
-    #     # initial_derivatives is a matrix that contains the error terms of all of the training samples which are the starting initial_derivatives
-
-    #     # NOTE: this algorithm can be implemented using recursion or using iterations
-    #     #  i chose to use iterations for performance purposes...
-
-    #     # since this is called backpropagation we have to go backwards through the layers
-
-    #     wgrads = []
-    #     bgrads = []
-    #     for index in range(len(self.layers) - 1, -1, -1):
-    #         layer_wgrads = (
-    #             1
-    #             / initial_derivatives.shape[0]
-    #             * np.matmul(self.outputs[index].T, initial_derivatives)
-    #         )
-    #         layer_bgrads = (
-    #             1 / initial_derivatives.shape[0] * np.sum(initial_derivatives, axis=0)
-    #         )
-            
-    #         initial_derivatives = np.matmul(
-    #             initial_derivatives, self.layers[index].weights.T
-    #         )
-    #         if self.layers[index].activation == "sigmoid":
-    #             initial_derivatives *= self.outputs[index] * (1 - self.outputs[index])
-    #         elif self.layers[index].activation == "relu":
-    #             initial_derivatives *= (self.outputs[index] > 0).astype(int)
-    #         elif self.layers[index].activation == "tanh":
-    #             initial_derivatives *= 1 - self.outputs[index] * self.outputs[index]
-
-    #         wgrads = [layer_wgrads] + wgrads
-    #         bgrads = [layer_bgrads] + bgrads
-
-    #     return wgrads, bgrads
-
-    def compile(self, optimizer: Optimizers, Cost: object):  # cost function object
+   
+    def compile(self, optimizer: object, Cost: object):  # cost function object
         self.optimizer = optimizer
         self.cost = Cost
         return self
@@ -90,13 +53,16 @@ class neural_network:
         return model, costs
     
     def summary(self):
+        """
+        print a summary of the model including weight biases and activations
+        """
         print("Summary of the neural network")
-        for layer in self.layers:
-            print(layer)
+        for layer_ in self.layers:
+            print(layer_)
             print("weights:")
-            print(layer.weights)
+            print(layer_.weights)
             print("biases:")
-            print(layer.biases)
+            print(layer_.biases)
             print()
             print('-'*50)
             print()
@@ -107,4 +73,7 @@ class neural_network:
         return self
 
     def predict(self, X: np.ndarray):
+        """
+        given an input dataset provide the output of the model
+        """
         return (self.forward(X) >= 0.5).astype(int)
