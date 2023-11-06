f3e3de1cdf1a6888f11cb2109a21a3d9cd7213ec
bayramenes <bayram.enes1928@gmail.com>
2023-11-06 16:50:33 +0300

    since we are passing an object we don't need methods to be static also instead of having a seperate .calculate() method i used the __call__ method

--- loss_funcs.py
+++ loss_funcs.py
@@ -7,14 +7,15 @@ class BinaryCrossEntropy:
 
     def __repr__(self) -> str:
         return "Binary Cross Entropy"
+    
+    
 
-    @staticmethod 
-    def calculate(output:np.ndarray,labels:np.ndarray) -> np.float64:
+    def __call__(self,output:np.ndarray,labels:np.ndarray) -> np.float64:
         return -1 / output.shape[0] * np.sum(labels * np.log(output + 1e-15) + (1 - labels) * np.log(1 - output + 1e-15))
     
     
-    @staticmethod
-    def initial_derivative(output:np.ndarray,labels:np.ndarray) -> np.ndarray:
+    
+    def initial_derivative(self,output:np.ndarray,labels:np.ndarray) -> np.ndarray:
         return output - labels
 
 
@@ -22,12 +23,12 @@ class BinaryCrossEntropy:
 class CategoricalCrossEntropy:
     def __repr__(self) -> str:
         return "Categorical Cross Entropy"
-    @staticmethod 
-    def calculate(output:np.ndarray,labels:np.ndarray) -> np.float64:
+    
+    def __call__(self,output:np.ndarray,labels:np.ndarray) -> np.float64:
         pass
 
-    @staticmethod
-    def initial_derivative():
+    
+    def initial_derivative(self):
         pass
 
 
@@ -35,11 +36,11 @@ class CategoricalCrossEntropy:
 class MSE:
     def __repr__(self) -> str:
         return "Mean Squared Error"
-    @staticmethod 
-    def calculate(output:np.ndarray,labels:np.ndarray) -> np.float64:
+    
+    def __call__(self,output:np.ndarray,labels:np.ndarray) -> np.float64:
         return (1/2) * np.mean(np.square(output - labels))
 
-    @staticmethod
-    def initial_derivative(output:np.ndarray,labels:np.ndarray) -> np.ndarray:
+    
+    def initial_derivative(self,output:np.ndarray,labels:np.ndarray) -> np.ndarray:
         return output - labels
         
