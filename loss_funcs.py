50648f221cfd2f861d10aca5d444fb6b9f98f7c5
bayramenes <bayram.enes1928@gmail.com>
2023-11-07 10:53:02 +0300

    added categorical cross entropy

--- loss_funcs.py
+++ loss_funcs.py
@@ -4,6 +4,14 @@
 import numpy as np
 
 class BinaryCrossEntropy:
+    """
+    implemenationn of binary cross entropy
+    this loss function shall be used with sigmoid activation function in the last layer with one output node
+
+    :param output: output of the last layer of the model
+    :param labels: labels of the training examples
+    :return: the loss value
+    """
 
     def __repr__(self) -> str:
         return "Binary Cross Entropy"
@@ -16,20 +24,45 @@ class BinaryCrossEntropy:
     
     
     def initial_derivative(self,output:np.ndarray,labels:np.ndarray) -> np.ndarray:
+        """
+        initial derivative to start backprop this will be the multiplicaition  of the derivative of the loss function and activation function of the last layer
+        :return float representing the initial derivative
+        
+        for a more involved and math heavy derivation check out this
+        https://math.stackexchange.com/questions/2503428/derivative-of-binary-cross-entropy-why-are-my-signs-not-right
+        """
         return output - labels
 
 
 
 class CategoricalCrossEntropy:
+    """
+    implemenation of categorical cross entropy 
+    this loss function shall be used with softmax activation function in the last layer that's why
+    it is also called softmax loss
+    the difference between it and binary cross entropy is that binary cross entropy is used with sigmoid activation function
+    and one node output where as categorical cross entropy works with multiple output nodes
+
+    :param output: output of the last layer of the model
+    :param labels: labels of the training examples
+    :return: the loss value
+    """
     def __repr__(self) -> str:
         return "Categorical Cross Entropy"
     
     def __call__(self,output:np.ndarray,labels:np.ndarray) -> np.float64:
-        pass
+        return np.sum(-labels * np.log(output + 1e-15))
 
     
-    def initial_derivative(self):
-        pass
+    def initial_derivative(self,output:np.ndarray , labels:np.ndarray) -> np.ndarray:
+        """
+        initial derivative to start backprop this will be the multiplicaition  of the derivative of the loss function and activation function of the last layer
+        :return float representing the initial derivative
+        
+        for a more involved and math heavy derivation check out this
+        https://towardsdatascience.com/derivative-of-the-softmax-function-and-the-categorical-cross-entropy-loss-ffceefc081d1
+        """
+        return output - labels
 
 
 
@@ -42,5 +75,15 @@ class MSE:
 
     
     def initial_derivative(self,output:np.ndarray,labels:np.ndarray) -> np.ndarray:
+        """
+        initial derivative to start backprop this will be the multiplicaition  of the derivative of the loss function and activation function of the last layer
+        :return float representing the initial derivative
+        
+        for a more involved and math heavy derivation check out this
+        https://math.stackexchange.com/questions/3713832/derivative-of-mean-squared-error
+        """
         return output - labels
+    
+
+
         
